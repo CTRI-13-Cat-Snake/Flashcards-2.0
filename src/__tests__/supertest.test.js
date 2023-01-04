a11ac7f8be44a4ee72cc354920061e37c09bd78f
Brian Jaber <brianjaber@gmail.com>
2023-01-04 09:21:02 -0800

    Currently figuring out how to test library route which needs to have a login

--- src/__tests__/supertest.test.js
+++ src/__tests__/supertest.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const fs = require('fs');
 // const { iteratorSymbol } = require('immer/dist/internal');
 
-const server = "http://localhost:3000";
+const server = "http://localhost:8080";
 
 describe('Route integration', () => {
   describe('/', () => {
@@ -10,6 +10,16 @@ describe('Route integration', () => {
       it('responds with 200 status and text/html content type', () => {
         return request(server)
           .get('/')
+          .expect('Content-Type', /text\/html/)
+          .expect(200);
+      })
+    })
+  })
+  describe('/library', () => {
+    describe('GET', () => {
+      it('responds with 200 status and text/html content type', () => {
+        return request(server)
+          .get('/library')
           .expect('Content-Type', /text\/html/)
           .expect(200);
       })
